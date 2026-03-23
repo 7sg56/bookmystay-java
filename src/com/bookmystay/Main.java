df1d30498c9114587ebe8addcb3b765d0eb1c4bd
Sourish Ghosh <sghosh.ile.7@gmail.com>
2026-03-23 09:16:50 +0530

    Initial commit: Bootstrap IntelliJ Java project

---
+++ src/com/bookmystay/Main.java
@@ -0,0 +1,10 @@
+package com.bookmystay;
+
+/**
+ * Main entry point for the BookMyStay Hotel Booking Management System.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to BookMyStay - Hotel Booking Management System");
+    }
+}
